d8807481cbc9a08ba3fd41356ecf8195420484ed
Fillipe Cordeiro <fillipeweb@gmail.com>
2012-07-27 16:40:26 -0300

    e6

---
+++ python_para_desenvolvedores/exercicios1/e6.py
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+# 6. Crie uma função que:
+# ▪ Receba uma lista de tuplas (dados), um inteiro (chave, zero por padrão igual) e um
+# booleano (reverso, falso por padrão).
+# ▪ Retorne dados ordenados pelo item indicado pela chave e em ordem decrescente se
+# reverso for verdadeiro.
+
+def sortTuple(list_ = []):
+    pass
